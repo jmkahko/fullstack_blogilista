7d34f0bb00b438eb5b02a4c5a3fc1de1b493becc
Janne Kähkönen <jm.kahkonen@gmail.com>
2023-09-17 15:32:31 +0300

    5.17: blogilistan end to end ‑testit, step1

--- app.js
+++ app.js
@@ -28,4 +28,9 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testsRouter = require('./controllers/tests')
+  app.use('/api/testing', testsRouter)
+}
+
 module.exports = app
